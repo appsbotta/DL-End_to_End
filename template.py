88208b0324069577209a7b4df2c8e4cad837661d
Lokesh <appsbotta@gmail.com>
2025-03-03 08:51:54 +0530

    setup.py

--- template.py
+++ template.py
@@ -12,20 +12,21 @@ project_name = "cnnClassifier"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
-    "src/__init__.py",
-    "src/components/__init__.py",
-    "src/utils/__init__.py",
-    "src/config/__init__.py",
-    "src/config/configuration.py"
-    "src/pipeline/__init__.py",
-    "src/entity/__init__.py"
-    "src/constants/__init__.py",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "dvc.yaml",
     "params.yaml",
     "requirements.txt",
     "setup.py",
-    "research/trails.ipynb"
+    "research/trails.ipynb",
+    "template/index.html",
 
 ]
 
